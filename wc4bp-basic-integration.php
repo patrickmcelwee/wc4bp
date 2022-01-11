07ac8161caa80d5b0ed96e4711fc6f1dfb52c869
Patrick McElwee <pmcelwee@gmail.com>
2022-01-11 06:00:02 -0500

    bump version number

--- wc4bp-basic-integration.php
+++ wc4bp-basic-integration.php
@@ -5,7 +5,7 @@
  * Description: Integrates WooCommerce with a BuddyPress social network
  * Author: ThemeKraft
  * Author URI: https://themekraft.com/products/woocommerce-buddypress-integration/
- * Version: 3.3.18
+ * Version: 3.3.18.2
  * Licence: GPLv3
  * Text Domain: wc4bp
  * Domain Path: /languages
